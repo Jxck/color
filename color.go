d3a3e0e1379e8c5e7c7982dfe2b570d9e1e3fc22
Jxck <block.rxckin.beats@gmail.com>
2013-10-11 23:19:59 +0900

    add color.go

---
+++ color.go
@@ -0,0 +1,54 @@
+package color
+
+import (
+	"fmt"
+	"strings"
+)
+
+type ColorCode string
+
+const (
+	red    ColorCode = "1;31"
+	green            = "1;32"
+	yellow           = "1;33"
+	blue             = "1;34"
+	pink             = "1;35"
+	cyan             = "1;36"
+	white            = "1;37"
+)
+
+func colorize(row string, color ColorCode) string {
+	return fmt.Sprintf("%s%s%s", "\033["+color+"m", row, "\033[0m")
+}
+
+func Red(row string) string {
+	return colorize(row, red)
+}
+
+func Green(row string) string {
+	return colorize(row, green)
+}
+
+func Yellow(row string) string {
+	return colorize(row, yellow)
+}
+
+func Blue(row string) string {
+	return colorize(row, blue)
+}
+
+func Pink(row string) string {
+	return colorize(row, pink)
+}
+
+func Cyan(row string) string {
+	return colorize(row, cyan)
+}
+
+func White(row string) string {
+	return colorize(row, white)
+}
+
+func Indent(v interface{}) string {
+	return strings.Replace(fmt.Sprintf("%v", v), "\n", "\n    ", -1)
+}
